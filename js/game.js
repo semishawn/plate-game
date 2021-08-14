2c3e58f488dd2f196406ef417bb1ef332ee9c32e
semishawn <45698497+semishawn@users.noreply.github.com>
2021-08-13 22:13:19 -0400

    semantics improvements

--- js/game.js
+++ js/game.js
@@ -4,34 +4,35 @@ function startGame() {
 	var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 	var middleWord = randomWord.slice(1, -1);
 
-	// Randomly generated letters
+	// Extract first, middle, and last letters from random word
 	letter1 = randomWord.slice(0, 1);
 	letter2 = middleWord.charAt(Math.floor(Math.random() * middleWord.length));
 	letter3 = randomWord.slice(-1);
 	var letString = (letter1 + letter2 + letter3).toUpperCase();
 
-	// Randomly generated numbers
-	function makeNumString() {
+	// Randomly generate numbers
+	function makeNumString(num) {
 		var string = "";
 		var digits = "0123456789";
-		for (var i = 0; i < 4; i++)
+		for (var i = 0; i < num; i++)
 			string += digits.charAt(Math.floor(Math.random() * digits.length));
 		return string;
 	}
-	var numString = makeNumString();
+	var numString = makeNumString(4);
 
-	// Display license plate number
+	// Display letters and numbers onto plate
 	$(".plate-chars1").html(letString);
 	$(".plate-chars2").html(numString);
 }
 
+// Start game on page load
 $(document).ready(function() {
 	startGame();
 });
 
 
 
-// submit word with play button
+// Submit word
 $(".submit-btn").click(function() {
 	var wordInput = $(".submit-box").val()
 
@@ -67,14 +68,14 @@ $(".submit-btn").click(function() {
 
 
 
-// If press enter
+// Run submit function on enter press
 $(document).keyup(function(e) {
 	if (e.keyCode === 13) $(".submit-btn").click();
 });
 
 
 
-// Skip function
+// Skip round with detour button
 $(".skip-btn").click(function() {
 	startGame();
 	$(".submit-box").val("");
@@ -82,7 +83,7 @@ $(".skip-btn").click(function() {
 
 
 
-// Skip function
+// Stop game with crash button
 $(".stop-btn").click(function() {
 	startGame();
 	$(".score-amount").html("0");
