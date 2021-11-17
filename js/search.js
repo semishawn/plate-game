307808746769fa4e97b9e24f91b66748427c77a9
semishawn <45698497+semishawn@users.noreply.github.com>
2021-11-17 15:59:47 -0500

    mobile optimizations

--- js/search.js
+++ js/search.js
@@ -15,53 +15,65 @@ $(".letter-input").on("propertychange input", function() {
 		var letter2 = $this.val().charAt(1).toLowerCase();
 		var letter3 = $this.val().charAt(2).toLowerCase();
 
+		// Filter words from letters
 		function filters(word) {
 			var cond1 = word.charAt(0) == letter1;
 			var cond2 = (word.slice(1, -1)).includes(letter2);
 			var cond3 = word.slice(-1) == letter3;
 			if (cond1 && cond2 && cond3) return true;
 		}
-
 		var filtered = wordList.filter(filters);
 
-		var sampleNum = 10;
-		var filteredHtml = "";
-		for (word in filtered.slice(0, sampleNum)) filteredHtml += ("<div class=\"filtered-word\">" + filtered[word] + "</div>");
+		// All words in HTML format
+		var filteredAllHtml = "";
+		for (word in filtered) filteredAllHtml += ("<div class=\"filtered-word\">" + filtered[word] + "</div>");
 
+		// Sample list of words in HTML format
+		var sampleNum = 10;
+		var filteredSampleHtml = "";
+		for (word in filtered.slice(0, sampleNum)) filteredSampleHtml += ("<div class=\"filtered-word\">" + filtered[word] + "</div>");
+
+		// Calculate word with most characters in filtered array
 		var longest = filtered.reduce(function(a, b) {
 			return a.length > b.length ? a : b;
 		});
 
+		// Calculate word with average amount of characters in filtered array
 		var middleLength = Math.floor(filtered.length / 2);
 		var filteredSorted = filtered.sort(function(a, b) {
 			return a.length - b.length
 		});
 		var middlest = filteredSorted[middleLength];
 
+		// Calculate worst with least characters in filtered array
 		var shortest = filtered.reduce(function(a, b) {
 			return a.length <= b.length ? a : b;
 		});
 
-		$(".actual-count").html(filtered.length + " words");
-		$(".word-list").html(filteredHtml);
-		if (filtered.length > sampleNum) $(".show-all-btn").show();
+		$(".actual-count").html(filtered.length);
+		var sampleList = $(".sample-word-list");
+		sampleList.html(filteredAllHtml);
+		if (sampleList[0].offsetHeight < sampleList[0].scrollHeight) $(".show-all-btn").css("visibility", "visible");
 		$(".actual-longest").html(longest);
 		$(".actual-middlest").html(middlest);
 		$(".actual-shortest").html(shortest);
-		$(".all-container").html(filteredHtml);
+		$(".all-word-list").html(filteredAllHtml);
 	} else {
-		$(".actual-count").html("? words");
-		$(".word-list").html("");
-		$(".show-all-btn").hide();
-		$(".actual-longest").html("...");
-		$(".actual-middlest").html("...");
-		$(".actual-shortest").html("...");
+		$(".actual-count").html("?");
+		$(".sample-word-list").html("");
+		$(".show-all-btn").css("visibility", "hidden");
+		$(".actual-longest").html("?");
+		$(".actual-middlest").html("?");
+		$(".actual-shortest").html("?");
 	}
 });
 
 
 
 $(".show-all-btn").click(function() {
-	$(".body-block").addClass("show-block");
-	$(".all-container").addClass("show-grid");
+	$("body").addClass("show-dialogue");
+});
+
+$(".close-all-btn").click(function() {
+	$("body").removeClass("show-dialogue");
 });
